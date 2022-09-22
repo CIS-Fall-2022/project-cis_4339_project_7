b56a22e79d59252aa59809a2080925f1417914c7
alirajgarah <77947215+alirajgarah@users.noreply.github.com>
2022-09-22 01:38:01 -0500

    Update organizationData.js

--- backend/models/organizationData.js
+++ backend/models/organizationData.js
@@ -1,1 +1,17 @@
 
+let organizationDataSchema = new Schema({ 
+    _id: {type: String, default: uuid.v1}, 
+    organizationName: {
+        type: String, 
+        require: true
+    },
+    servicesProvided: [{
+        type: ObjectID,
+        ref: 'serviceData'
+    }]
+    },{
+        collection: 'organizationData',
+        timestamps: true 
+}); 
+
+module.exports = mongoose.model(organizationDataSchema)
