ad504b549b6fd131c611f754435734a461d34058
alirajgarah <77947215+alirajgarah@users.noreply.github.com>
2022-09-28 00:41:18 -0500

    added serviceDataRoute

---
+++ backend/routes/serviceDataRoute.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+
+//importing data model schemas
+let { servicedata } = require("../models/serviceData"); 
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    servicedata.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => { 
+    servicedata.find({ _id: req.params.id }, (error, data) => {
+        if (error) {
+            return next(error)
+        } else {
+            res.json(data)
+        }
+    })
+});
+
+
+
+//POST
+router.post("/", (req, res, next) => { 
+    servicedata.create( 
+        req.body, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//PUT
+router.put("/:id", (req, res, next) => {
+    servicedata.findOneAndUpdate(
+        { _id: req.params.id },
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+module.exports = router;
