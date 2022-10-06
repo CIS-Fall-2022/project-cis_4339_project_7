4fcbfbba34d5a87a8a591f763cf9c2f02751dc6e
nibodiongan <nibodiongan@gmail.com>
2022-10-06 12:53:31 -0500

    added references

--- backend/routes/organizationData.js
+++ backend/routes/organizationData.js
@@ -51,6 +51,8 @@ router.get("/id/:id", (req, res, next) => {
 });
 
 //GET services provided
+// reference https://medium.com/fasal-engineering/fetching-data-from-different-collections-via-mongodb-aggregation-operations-with-examples-a273f24bfff0
+
 router.get("/services/:id", (req, res, next) => { 
     organizationdata.aggregate([
         // $match finds an exact match base on the parameters that you set
@@ -85,6 +87,7 @@ router.get("/services/:id", (req, res, next) => {
 });
 
 //GET all events for organization
+// reference https://stackoverflow.com/questions/37202585/check-if-value-exists-in-array-field-in-mongodb
 router.get("/events/:id", (req, res, next) => { 
     eventdata.find( 
         { organizations: req.params.id }, 
@@ -101,6 +104,7 @@ router.get("/events/:id", (req, res, next) => {
 });
 
 //GET clients for organization
+//https://stackoverflow.com/questions/37202585/check-if-value-exists-in-array-field-in-mongodb
 router.get("/clients/:id", (req, res, next) => { 
     primarydata.find( 
         { clientOfOrgs: req.params.id }, 
@@ -134,6 +138,7 @@ router.put("/:id", (req, res, next) => {
 });
 
 //add serviceID to array
+// reference https://www.mongodb.com/docs/manual/reference/method/db.collection.findOneAndUpdate/
 router.put('/addservicefororg/:id', (req, res, next) => {
     organizationdata.findOneAndUpdate({ organizationID: req.params.id, 
         servicesProvided: {$not: { $in: req.body.serviceID }} }, 
@@ -151,6 +156,7 @@ router.put('/addservicefororg/:id', (req, res, next) => {
 });
 
 // delete services in org
+// reference https://www.mongodb.com/docs/manual/reference/method/db.collection.findOneAndUpdate/
 router.put('/removeservice/:id', (req, res, next) => {
     organizationdata.findOneAndUpdate({ organizationID: req.params.id,
         servicesProvided : {$in: req.body.serviceID} }, 
@@ -168,6 +174,7 @@ router.put('/removeservice/:id', (req, res, next) => {
 });
 
 //delete organization by ID
+// reference https://www.mongodb.com/docs/manual/reference/method/db.collection.findOneAndUpdate/
 router.delete('/removeorg/:id', (req, res, next) => {
     organizationdata.findOneAndRemove({ organizationID: req.params.id}, (error, data) => {
         if (error) {
