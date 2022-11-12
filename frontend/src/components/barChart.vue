f80e2288e4f57e0f08001c3f34610227dcd7476c
nibodiongan <77943402+nibodiongan@users.noreply.github.com>
2022-11-12 01:34:59 -0600

    dashboard with bar chart
    connected backend using vite_root_api

---
+++ frontend/src/components/barChart.vue
@@ -0,0 +1,41 @@
+<template>
+    <canvas ref="myChart"></canvas>
+  </template>
+  
+  <script>
+  import { Chart, registerables } from 'chart.js'
+  Chart.register(...registerables);
+  export default {
+    props: {
+      label: {
+        type: Array,
+      },
+      chartData: {
+        type: Array,
+      },
+    },
+    async mounted() {
+      console.log(this.chartData);
+      await new Chart(this.$refs.myChart, {
+        type: "bar",
+        data: {
+          labels: this.label,
+          datasets: [
+            {
+              label: "Clients",
+              backgroundColor: "rgba(255,99,71 , 0.9 )",
+              data: this.chartData,
+            },
+          ],
+        },
+        options: {
+          scales: {
+              y: {
+                  min: 1
+              }
+          }
+        }
+      });
+    },
+  };
+  </script>
--- frontend/src/components/dashboard.vue
+++ frontend/src/components/dashboard.vue
@@ -1,16 +1,78 @@
 <template>
-  <main>
-    <div>
-      <h1 class="font-bold text-4xl text-red-700 tracking-widest text-center mt-10">Welcome</h1>
-    </div>
-  </main>
+  <div>
+    <h2> Events scheduled from the past 2 months</h2>
+    <AttendingBar
+              v-if="!loading && !error"
+              :label="labels"
+              :chart-data="chartData"
+            ></AttendingBar>
+
+            <div class="mt-40" v-if="loading">
+              <p
+                class="
+                  text-6xl
+                  font-bold
+                  text-center text-gray-500
+                  animate-pulse
+                "
+              >
+                Loading...
+              </p>
+            </div>
+  </div>
 </template>
-<script>
-export default {
-  methods: {
-    routePush(routeName) {
-      this.$router.push({ name: routeName });
+  
+  <script>
+  import AttendingBar from './barChart.vue';
+  import axios from "axios";
+  export default {
+    components:{
+      AttendingBar, 
+    },
+    data(){
+      return {
+        labels: [],
+        chartData :[],
+        loading: false,
+        error: null,
+      }
+    },
+methods: {
+    async fetchData() {
+      try {
+        this.error = null;
+        this.loading = true;
+        const url = import.meta.env.VITE_ROOT_API + '/eventData/last2months';
+        const response = await axios.get(url);
+        //"re-organizing" - mapping json from the response
+        this.labels = response.data.map((item) => item.eventName);
+        this.chartData = response.data.map((item) => item.number_of_clients);
+      } catch (err) {
+        if (err.response) {
+          // client received an error response (5xx, 4xx)
+          this.error = {
+            title: "Server Response",
+            message: err.message,
+          };
+        } else if (err.request) {
+          // client never received a response, or request never left
+          this.error = {
+            title: "Unable to Reach Server",
+            message: err.message,
+          };
+        } else {
+          // There's probably an error in your code
+          this.error = {
+            title: "Application Error",
+            message: err.message,
+          };
+        }
+      }
+      this.loading = false;
     },
   },
+  mounted() {
+    this.fetchData();
+  },
 };
-</script>
+  </script>
