901e437d68bada6969b05bda31a9e3fae422184d
alirajgarah <77947215+alirajgarah@users.noreply.github.com>
2022-11-14 01:41:09 -0600

    Update findClient.vue

--- frontend/src/components/findClient.vue
+++ frontend/src/components/findClient.vue
@@ -72,7 +72,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-x-6 gap-y-10">
       <div class="ml-10">
         <h2 class="text-2xl font-bold">List of Clients</h2>
-        <h3 class="italic">Click table row to edit/display an entry</h3>
+        <h3 class="italic">Click table row to edit/display/delete an entry</h3>
       </div>
       <div class="flex flex-col col-span-2">
         <table class="min-w-full shadow-md rounded">
